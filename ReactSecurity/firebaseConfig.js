b55732f517247e6380a9466a921470443f62c317
Jajaider24 <j41d3rr@gmail.com>
2025-10-28 21:14:12 -0500

    Intento de firebase

---
+++ ReactSecurity/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA7UmElNlXAnPfDHvfJTcWvvh9Vka8jJ8",
+  authDomain: "proyecto-react-5fc5b.firebaseapp.com",
+  projectId: "proyecto-react-5fc5b",
+  storageBucket: "proyecto-react-5fc5b.firebasestorage.app",
+  messagingSenderId: "429402941051",
+  appId: "1:429402941051:web:c28445382064e5bb41e4de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
